b964df12b975dca3c692bed7fd8c373e895d2448
Marcelo Carrión <zane5915@gmail.com>
2024-04-26 18:35:26 -0500

    Update PatientController.java

--- src/main/java/com/genetech/genetech_backend/controller/PatientController.java
+++ src/main/java/com/genetech/genetech_backend/controller/PatientController.java
@@ -3,6 +3,7 @@ package com.genetech.genetech_backend.controller;
 import com.genetech.genetech_backend.dto.PatientDto;
 import com.genetech.genetech_backend.model.Patient;
 import com.genetech.genetech_backend.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PatientController {
+    @Autowired
     private PatientService patientService;
 
     //Registrar paciente
