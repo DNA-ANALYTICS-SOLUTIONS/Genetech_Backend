28fea9354fe6229fd2a148963f80e6a676ab3b13
Luis Derek <derek04.bs@gmail.com>
2024-04-27 23:21:24 -0500

    Update GenetechBackendApplication.java
    other testing commit

--- src/main/java/com/genetech/genetech_backend/GenetechBackendApplication.java
+++ src/main/java/com/genetech/genetech_backend/GenetechBackendApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GenetechBackendApplication {
-//Testing for Brenda xd
+
+
     public static void main(String[] args) {
         SpringApplication.run(GenetechBackendApplication.class, args);
     }
