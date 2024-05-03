a1ec1e254500acb1a5b3b3237278a4fe82d027a6
Marcelo Carrión <zane5915@gmail.com>
2024-05-02 22:48:24 -0500

    Create SecurityConfig.java

---
+++ src/main/java/com/genetech/genetech_backend/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.genetech.genetech_backend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+        return http
+                .csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+}
